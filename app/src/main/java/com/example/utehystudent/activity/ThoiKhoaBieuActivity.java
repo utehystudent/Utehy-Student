1ae60cf870ffbdcd0874762ce0233029742554b1
viethoang207 <vieth@mirabo.com.vn>
2022-06-06 16:44:26 +0700

    code chức năng cập nhật TKB

--- app/src/main/java/com/example/utehystudent/activity/ThoiKhoaBieuActivity.java
+++ app/src/main/java/com/example/utehystudent/activity/ThoiKhoaBieuActivity.java
@@ -1,14 +1,17 @@
 package com.example.utehystudent.activity;
 
 import android.app.Dialog;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -19,8 +22,7 @@ import com.example.utehystudent.R;
 import com.example.utehystudent.adapters.ThoiKhoaBieuAdapter;
 import com.example.utehystudent.model.Schedule_detail;
 import com.example.utehystudent.model.ThoiKhoaBieu;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
@@ -32,8 +34,9 @@ public class ThoiKhoaBieuActivity extends AppCompatActivity {
     ThoiKhoaBieuAdapter tkbAdapter;
     ArrayList<ThoiKhoaBieu> listTKB;
     FirebaseFirestore db;
-    ProgressDialog progressDialog;
-
+    String classID = "";
+    View parentLayout;
+    ProgressBar prgBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +45,7 @@ public class ThoiKhoaBieuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_thoi_khoa_bieu);
 
         Init();
-
+        parentLayout = findViewById(android.R.id.content);
     }
 
     private void Init() {
@@ -52,9 +55,7 @@ public class ThoiKhoaBieuActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("THỜI KHÓA BIỂU");
         toolbar.setNavigationOnClickListener(v -> finish());
 
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("Đang tải dữ liệu...");
-        progressDialog.show();
+        prgBar = findViewById(R.id.TKB_prgBar);
 
         db = FirebaseFirestore.getInstance();
 
@@ -64,16 +65,17 @@ public class ThoiKhoaBieuActivity extends AppCompatActivity {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         rcv.setLayoutManager(linearLayoutManager);
         tkbAdapter = new ThoiKhoaBieuAdapter(this, listTKB);
-        rcv.setAdapter(tkbAdapter);
 
         //get data
         getData();
-
     }
 
     private void getData() {
+        rcv.setVisibility(View.GONE);
+        prgBar.setVisibility(View.VISIBLE);
+        listTKB.clear();
         SharedPreferences pref = getSharedPreferences("User", Context.MODE_PRIVATE);
-        String classID = pref.getString("class_ID", "");
+        classID = pref.getString("class_ID", "");
         listTKB.add(new ThoiKhoaBieu("Monday", "", "", ""));
         listTKB.add(new ThoiKhoaBieu("Tuesday", "", "", ""));
         listTKB.add(new ThoiKhoaBieu("Wednesday", "", "", ""));
@@ -89,21 +91,20 @@ public class ThoiKhoaBieuActivity extends AppCompatActivity {
                     .collection("Schedule_detail")
                     .document(weekday)
                     .get()
-                    .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                        @Override
-                        public void onSuccess(DocumentSnapshot documentSnapshot) {
-                            Schedule_detail detail = documentSnapshot.toObject(Schedule_detail.class);
-                            tkb.setSang(detail.getMorning());
-                            tkb.setChieu(detail.getAfternoon());
-                            tkb.setToi(detail.getEvening());
-                            Log.d("tkb", "onSuccess: " + tkb.toString());
-                            tkbAdapter.notifyDataSetChanged();
-                        }
+                    .addOnSuccessListener(documentSnapshot -> {
+                        Schedule_detail detail = documentSnapshot.toObject(Schedule_detail.class);
+                        tkb.setSang(detail.getMorning());
+                        tkb.setChieu(detail.getAfternoon());
+                        tkb.setToi(detail.getEvening());
+                        Log.d("tkb", "onSuccess: " + tkb.toString());
+                        tkbAdapter.notifyDataSetChanged();
                     });
         }
-        progressDialog.dismiss();
+        rcv.setAdapter(tkbAdapter);
+        prgBar.setVisibility(View.GONE);
+        rcv.setVisibility(View.VISIBLE);
     }
-    
+
     public void clickItemTKB(ThoiKhoaBieu tkb) {
         Dialog dialog = new Dialog(this);
         dialog.setContentView(R.layout.dialog_chitiet_tkb);
@@ -111,16 +112,57 @@ public class ThoiKhoaBieuActivity extends AppCompatActivity {
         EditText edtSang = dialog.findViewById(R.id.dialogCTTKB_edtTenMHSang);
         EditText edtChieu = dialog.findViewById(R.id.dialogCTTKB_edtTenMHChieu);
         EditText edtToi = dialog.findViewById(R.id.dialogCTTKB_edtTenMHToi);
+        Button btnLuu = dialog.findViewById(R.id.dialogCTTKB_btnLuu);
 
         tvThu.setText(getThu(tkb.getThu()));
         edtSang.setText(tkb.getSang());
         edtChieu.setText(tkb.getChieu());
         edtToi.setText(tkb.getToi());
 
+        btnLuu.setOnClickListener(it -> {
+            tkb.setSang(edtSang.getText().toString().trim());
+            tkb.setChieu(edtChieu.getText().toString().trim());
+            tkb.setToi(edtToi.getText().toString().trim());
+
+            if (edtSang.getText().toString().equals("")
+                    || edtSang.getText().toString().equalsIgnoreCase("nghỉ")
+                    || edtSang.getText().toString().equalsIgnoreCase("nghi")) {
+                tkb.setSang("Nghỉ");
+            }
+            if (edtChieu.getText().toString().equals("")
+                    || edtChieu.getText().toString().equalsIgnoreCase("nghỉ")
+                    || edtChieu.getText().toString().equalsIgnoreCase("nghi")) {
+                tkb.setChieu("Nghỉ");
+            }
+            if (edtToi.getText().toString().equals("")
+                    || edtToi.getText().toString().equalsIgnoreCase("nghỉ")
+                    || edtToi.getText().toString().equalsIgnoreCase("nghi")) {
+                tkb.setToi("Nghỉ");
+            }
+
+            //update dữ liệu lên firestore
+            db.collection("Schedule")
+                    .document(classID)
+                    .collection("Schedule_detail")
+                    .document(tkb.getThu())
+                    .update("morning", tkb.getSang()
+                            , "afternoon", tkb.getChieu()
+                            , "evening", tkb.getToi())
+                    .addOnSuccessListener(unused -> {
+                        Snackbar.make(parentLayout, "Cập nhật thời khóa biểu thành công", Snackbar.LENGTH_SHORT).show();
+                        getData();
+                        dialog.dismiss();
+                    })
+                    .addOnFailureListener(e -> {
+                        Toast.makeText(ThoiKhoaBieuActivity.this, "Đã có lỗi xảy ra. Vui lòng thử lại sau", Toast.LENGTH_SHORT).show();
+                        Log.e(TAG, "clickItemTKB: " + e.getMessage());
+                        dialog.dismiss();
+                        return;
+                    });
+        });
 
         dialog.create();
         dialog.show();
-
     }
 
     private String getThu(String s) {
@@ -147,7 +189,9 @@ public class ThoiKhoaBieuActivity extends AppCompatActivity {
             case "Sunday":
                 rs = "Chủ nhật";
                 break;
-            default: rs = ""; break;
+            default:
+                rs = "";
+                break;
         }
         return rs;
     }
